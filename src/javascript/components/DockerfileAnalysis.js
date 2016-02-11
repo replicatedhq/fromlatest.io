e991e234dca18663fd0e80c0c7e9b1c7f6d186dd
Marc Campbell <marc.e.campbell@gmail.com>
2016-02-11 10:28:45 -0800

    Expand and collapse groups

--- src/javascript/components/DockerfileAnalysis.js
+++ src/javascript/components/DockerfileAnalysis.js
@@ -13,8 +13,18 @@ var DockerfileAnalysis = React.createClass({
 
   getInitialState: function() {
     return {
-      expanded: []
+      expandedItems: [],
+      expandedGroups: ['possibleBugs', 'optimization', 'clarity', 'deprecation']
     };
+  },
+
+  onGroupToggle: function(groupName) {
+    if (_.includes(this.state.expandedGroups, groupName)) {
+      this.state.expandedGroups = _.without(this.state.expandedGroups, groupName);
+    } else {
+      this.state.expandedGroups.push(groupName);
+    }
+    this.setState({expandedGroups: this.state.expandedGroups});
   },
 
   onItemExpand: function(item) {
@@ -23,9 +33,9 @@ var DockerfileAnalysis = React.createClass({
   },
 
   onItemCollapse: function(item) {
-    var expanded = this.state.expanded;
-    expanded = _.without(expanded, item);
-    this.setState({expanded: expanded});
+    var expandedItems = this.state.expandedItems;
+    expandedItems = _.without(expandedItems, item);
+    this.setState({expandedItems: expandedItems});
   },
 
   render: function() {
@@ -50,7 +60,7 @@ var DockerfileAnalysis = React.createClass({
       var node = (
         <AnalysisItem
           active={(this.props.selectionStart <= item.line) && (this.props.selectionStop >= item.line)}
-          expanded={_.includes(this.state.expanded, item)}
+          expanded={_.includes(this.state.expandedItems, item)}
           key={item.line + item.title}
           item={item} onExpand={this.onItemExpand}
           onCollapse={this.onItemCollapse}/>
@@ -66,23 +76,78 @@ var DockerfileAnalysis = React.createClass({
       }
     }.bind(this));
 
+    var groupHeaderIconClasses = {
+      'possibleBugs':
+        classNames('fa',
+          {'fa-caret-down': _.includes(this.state.expandedGroups, 'possibleBugs'),
+           'fa-caret-right': !_.includes(this.state.expandedGroups, 'possibleBugs')
+         }),
+      'optimization':
+        classNames('fa',
+          {'fa-caret-down': _.includes(this.state.expandedGroups, 'optimization'),
+           'fa-caret-right': !_.includes(this.state.expandedGroups, 'optimization')
+         }),
+      'clarity':
+        classNames('fa',
+          {'fa-caret-down': _.includes(this.state.expandedGroups, 'clarity'),
+           'fa-caret-right': !_.includes(this.state.expandedGroups, 'clarity')
+         }),
+      'deprecation':
+        classNames('fa',
+          {'fa-caret-down': _.includes(this.state.expandedGroups, 'possibleBugs'),
+           'fa-caret-right': !_.includes(this.state.expandedGroups, 'deprecation')
+         })
+    };
+
     return (
       <div className="analysis">
-        <div className={classNames({hidden: nodes.possibleBugs.length === 0})}>
-          <h3>Possible Bugs</h3>
-          {nodes.possibleBugs}
+        <div>
+          <h3>
+            <a href="#" onClick={this.onGroupToggle.bind(this, 'possibleBugs')}>
+              <i className={groupHeaderIconClasses.possibleBugs} />
+              {' '}
+              Possible Bugs - {nodes.possibleBugs.length} found
+            </a>
+          </h3>
+          <div className={classNames({'hidden': !_.includes(this.state.expandedGroups, 'possibleBugs')})}>
+            {nodes.possibleBugs}
+          </div>
         </div>
-        <div className={classNames({hidden: nodes.optimization.length === 0})}>
-          <h3>Optimization</h3>
-          {nodes.optimization}
+        <div>
+          <h3>
+            <a href="#" onClick={this.onGroupToggle.bind(this, 'optimization')}>
+              <i className={groupHeaderIconClasses.optimization} />
+              {' '}
+              Optimization - {nodes.optimization.length} found
+            </a>
+          </h3>
+          <div className={classNames({'hidden': !_.includes(this.state.expandedGroups, 'optimization')})}>
+            {nodes.optimization}
+          </div>
         </div>
-        <div className={classNames({hidden: nodes.clarity.length === 0})}>
-          <h3>Clarity</h3>
-          {nodes.clarity}
+        <div>
+          <h3>
+            <a href="#" onClick={this.onGroupToggle.bind(this, 'clarity')}>
+              <i className={groupHeaderIconClasses.clarity} />
+              {' '}
+              Clarity - {nodes.clarity.length} found
+            </a>
+          </h3>
+          <div className={classNames({'hidden': !_.includes(this.state.expandedGroups, 'clarity')})}>
+            {nodes.clarity}
+          </div>
         </div>
-        <div className={classNames({hidden: nodes.deprecation.length === 0})}>
-          <h3>Deprecation</h3>
-          {nodes.deprecation}
+        <div>
+          <h3>
+            <a href="#" onClick={this.onGroupToggle.bind(this, 'deprecation')}>
+              <i className={groupHeaderIconClasses.deprecation} />
+              {' '}
+              Deprecation - {nodes.deprecation.length} found
+            </a>
+          </h3>
+          <div className={classNames({'hidden': !_.includes(this.state.expandedGroups, 'deprecation')})}>
+            {nodes.deprecation}
+          </div>
         </div>
       </div>
     );
