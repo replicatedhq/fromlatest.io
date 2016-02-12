7bb6af0c32d3bbb05d447aed2458b1fe1a997083
Marc Campbell <marc.e.campbell@gmail.com>
2016-02-11 21:41:40 -0800

    Add links to github repos on about page

--- src/javascript/components/About.js
+++ src/javascript/components/About.js
@@ -15,6 +15,17 @@ var About = React.createClass({
               <li>Smallest possible images</li>
             </ul>
           </p>
+          <h2><i className="fa fa-github" /> Contribute</h2>
+          <div className="row">
+            <div className="col-md-6">
+              The linter is available at replicatedhq/dockerfilelint<br />
+              <iframe src="https://ghbtns.com/github-btn.html?user=replicatedhq&repo=dockerfilelint&type=star&count=true&size=large" frameBorder="0" scrolling="0" width="158px" height="30px"></iframe>
+            </div>
+            <div className="col-md-6">
+              This site is available at replicatedhq/fromlatest.io<br />
+              <iframe src="https://ghbtns.com/github-btn.html?user=replicatedhq&repo=fromlatest.io&type=star&count=true&size=large" frameBorder="0" scrolling="0" width="158px" height="30px"></iframe>
+            </div>
+          </div>
         </div>
       </div>
     );
